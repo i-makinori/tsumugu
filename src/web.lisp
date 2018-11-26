6446563abfa76708d08173f4b0af80dd331a91bf
i-makinori <maau3p@gmail.com>
2018-11-26 14:27:54 +0900

    [Add] route for update files to server(1)
    [todo]
    - clean up
    - test any else typed file
    - data abstraction
    - connect to db for binary-datas
    - utf-8 support

--- src/web.lisp
+++ src/web.lisp
@@ -7,7 +7,8 @@
         :tsumugu.db
         :tsumugu.model
         :datafly
-        :sxql)
+        :sxql
+        :clack)
   (:export :*web*))
 (in-package :tsumugu.web)
 
@@ -46,8 +47,76 @@
     (format nil "articles: ~A" articles)))
 
 
-;; (print (list-articles))
-;; (print (article-head-list (list-articles)))
+
+;; uploader such as image-files
+;; [ref]
+;; http://progn.hateblo.jp/entry/20170131/1485855797
+;; https://github.com/edicl/flexi-streams/issues/28
+;; 
+
+
+(defroute "/post-form" ()
+  (let ((form-html "
+<form action=\"/post\" method=\"POST\" enctype=\"multipart/form-data\">
+  <input type=\"text\" name=\"message\" value=\"message here\" />
+  <input type=\"file\" name=\"file\" />
+  <input type=\"submit\" value=\"Send\" />
+</form>"))
+    (format nil "~A" form-html)))
+
+
+(defroute ("/post" :method :POST) (&key |file| |message|)
+  (let* ((time (write-to-string (get-universal-time)))
+         (fname (merge-pathnames
+                 (format nil "~A.jpg" time)
+                 *static-directory*)))
+    #|
+    (with-open-file (out fname
+                         :direction :output
+                         :element-type '(unsigned-byte 8)
+                         :if-does-not-exist :create)
+    (write-sequence (slot-value |file| 'flexi-streams::vector) out))
+    |#
+    (setf *sample-file* |file|)
+    (setf *sample-message* |message|)
+    (format nil "~A" |file|)))
+
+
+
+(with-open-file (out-stream #P"~/Downloads/hoge.png"
+                            :direction :output
+                            :element-type '(unsigned-byte 8)
+                            :if-does-not-exist :create
+                            :if-exists :overwrite)
+  (write-sequence (slot-value (car *sample-file*) 'flexi-streams::vector)
+                  out-stream))
+
+#|
+
+(print *sample-file*)
+;; (#<FLEXI-STREAMS::VECTOR-INPUT-STREAM {10028B5893}>
+;;  #<HASH-TABLE :TEST EQUAL :COUNT 2 {10028B5393}>
+;;  #<HASH-TABLE :TEST EQUAL :COUNT 2 {10028B4D43}>)
+
+;; FLEXI-STREAMS::VECTOR-INPUT-STREAM
+
+
+
+;; (print (flexi-streams::stream-file-position (car *sample-file*)))
+;; (print (flexi-streams::stream-read-byte (car *sample-file*)))
+
+
+(with-open-stream (s (car *sample-file*))
+  (let (c)
+    (loop for c = (read-byte s nil)
+       while c
+       do (format t "~S " c))))
+
+
+(print (slot-value (car *sample-file*) 'flexi-streams::vector))
+(write-sequence (slot-value (car *sample-file*) 'flexi-streams::vector) *standard-output*)
+
+|#
 
 
 ;;
