5d3391a312728e3d71baf9e27aa5468d4da8e35c
i-makinori <maau3p@gmail.com>
2018-12-10 14:49:52 +0900

    Change structure of sqlite database

--- db/initialize.sql
+++ db/initialize.sql
@@ -21,51 +21,56 @@ eath date of text is such as : "YYYY-MM-DD HH:MM:SS.SSS{{+ or -}}TIME:ZOON"
 */
 
 
-CREATE table craftsmans(
-       id int NOT NULL,
+CREATE table observer(
+       num_enuem int NOT NULL,
+       cosmic_link carchar(28) NOT NULL,
        name varchar(28) NOT NULL,
        password varchar(144) NOT NULL,
        --
        email varchar(144),
        --
-       UNIQUE(id));
+       UNIQUE(num_enuem, cosmic_link));
 
-INSERT INTO craftsmans(id, name, password, email) VALUES
-       (0, "nil user", "password", "email@*.com");
-INSERT INTO craftsmans(id, name, password, email) VALUES
-       (1, "next of nil", "password2", "email@*2.com");
+INSERT INTO observer(num_enuem, cosmic_link, name, password, email) VALUES
+       (0, "neko_neko_chaos" , "chaos shin", "dolphins", "email@*.com");
+INSERT INTO observer(num_enuem, cosmic_link, name, password, email) VALUES
+       (1, "the-atmos", "atmos", "atoms_password", "email@*2.com");
 
 
 CREATE table article(
-       id int NOT NULL,
+       num_enuem int NOT NULL,
+       cosmic_link TEXT, -- === uri
        --
-       user_id int NOT NULL,
+       observer_id int NOT NULL,
+       authorities TEXT, -- 000 to 777 to each observer groupus
        updated_at TEXT,
        --
        title TEXT,
        tags TEXT,
        contents TEXT,
        --
-       UNIQUE(id));
+       UNIQUE(num_enuem, cosmic_link));
 
-INSERT INTO article(id, user_id, updated_at, title, tags, contents) VALUES
-       (0, 0, "bifore big bang", "title", "tags", "1st article am this");
-INSERT INTO article(id, user_id, updated_at, title, tags, contents) VALUES
-       (1, 1, "2018-11-25 22:22:22.22+09:00", "hell-oww I am next-of-nil", "tags hello-world", "this is my first article... wow.");
-INSERT INTO article(id, user_id, updated_at, title, tags, contents) VALUES
-       (2, 0, "2018-11-26 14:40:00.00-12:12", "respons to 1st article", "tags hell-world", "wellcome to hell, Mr.next-of-nil.");
+INSERT INTO article(num_enuem, observer_id, authorities, updated_at, cosmic_link, title, tags, contents) VALUES
+       (0, 0, "", "bifore big bang", "this_is_id", "this_is_title", "this_is_tag", "hell o this is first article content");
+INSERT INTO article(num_enuem, observer_id, authorities, updated_at, cosmic_link, title, tags, contents) VALUES
+       (1, 1, "", "2018-11-25 22:22:22.22+09:00", "hello-dier", "hello dier", "hell", "this is my first article... wow");
+
+
+
+
 
 
 CREATE table blobs(
        -- (self.id) is also pointerd from (\c1 ::= (1-row-of (article::table))).contents
        -- if (c1.tags contain "*blob*")
        --
-       id int NOT NULL,
-       file_name TEXT,
+       num_enuem int NOT NULL,
+       cosmic_link TEXT, -- file name
        bytearray_contents BLOB,
        --
-       UNIQUE(id));
+       UNIQUE(num_enuem, cosmic_link));
 
-INSERT INTO blobs(id, file_name, bytearray_contents) VALUES
+INSERT INTO blobs(num_enuem, cosmic_link, bytearray_contents) VALUES
        (0, "test_image.png", "");
 ;;
